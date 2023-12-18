2e79993726ee23a9a190190548314a37af34258f
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2023-12-17 19:20:41 -0500

    add start files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component, Fragment } from 'react';
+
+export class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div>React homework template</div>
+      </Fragment>
+    );
+  }
+}
